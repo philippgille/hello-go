dc7786cc136f75acfdeeda66aab8e819b86075ae
Philipp Gille <philipp.gille@gmail.com>
2018-04-12 22:24:14 +0200

    Add concurrency.go
    - Code for the "concurrency" chapter
    - Not finished yet - code until including slide 3 of 11

---
+++ concurrency/concurrency.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// calling this function in a goroutin executes it in a lightweight thread
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func myGoroutine() {
+	go say("world") // executes "say" in a separate goroutine and continues immediately
+	say("hello")
+}
+
+// ========
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func myChannel() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+}
+
+func main() {
+	myGoroutine()
+
+	myChannel()
+
+	// Buffered channels
+	ch := make(chan int, 2)
+	ch <- 1 // Doesn't block
+	ch <- 2 // Doesn't block
+	// Sending another message through the channel would overfill the buffer and block the current thread
+	// (only until another goroutine reads from the channel, which is not the case in this example code)
+	// ch <- 3
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
